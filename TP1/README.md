bc9dabcc5a7b9cfb69bfbae930b3af36609ee8d6
Utilisateur <Utilisateur@172.29.98.45>
2017-09-22 17:20:56 -0400

    Ajout des méthodes getSalary et roundToFive

---
+++ TP1/README.md
@@ -0,0 +1,4 @@
+# A17-Team11
+# allo un test
+
+##Ceci est un autre test de sous category par Jade
---
+++ TP1/src/Application.java
@@ -0,0 +1,82 @@
+/**
+ * Created by davidboutet on 17-09-18.
+ */
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+import manage.file.*;
+import net.sf.json.*;
+
+
+import com.inf.Employe;
+
+
+public class Application {
+    static ArrayList<Employe> finalEmployeList = new ArrayList<Employe>();
+    public static void main(String[] args){
+        
+        String filePath = args[0];
+        String outputFile = args[1];
+
+        //load external json file(name in argument) and return it as a JSONObject
+        JSONObject jsonObject = getJsonFromFile(filePath);
+        
+        //parse JSON object to required output format
+        jsonToEmployeList(jsonObject);
+
+        //write JSONObject to json file pass in argument
+        println("JSON writing: "+writeJson(jsonObject, outputFile));
+    }
+
+    public static JSONObject getJsonFromFile(String source){
+        JSONObject jsonObject = null;
+        try{
+            String myJSON = FileManager.createStringFromFileContent(source, "");
+            jsonObject = JSONObject.fromObject(myJSON);
+        }catch(FileNotFoundException fe){
+            fe.printStackTrace();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
+    //A voir si on devrait le mettre dans la classe Employe.java
+    public static Boolean jsonToEmployeList(JSONObject jsonObject){
+        Boolean succeed = false;
+        try{
+            Integer department_type = jsonObject.getInt("type_departement");
+            Double hourly_rate_min = jsonObject.getDouble("taux_horaire_min"); 
+            Double hourly_rate_max = jsonObject.getDouble("taux_horaire_max");
+            JSONArray employeArray = jsonObject.getJSONArray("employes");
+            for (int i = 0; i<employeArray.size(); i++){
+                JSONObject employe = (JSONObject)employeArray.get(i);
+                Employe e = new Employe(employe.getString("nom"), department_type, hourly_rate_min, hourly_rate_max);
+
+                finalEmployeList.add(e);
+            }
+            succeed = true;
+        }catch (Exception e){
+            println(e);
+        }
+
+        return succeed;
+    }
+
+    public static Boolean writeJson(JSONObject json, String filename){
+        Boolean succeed = false;
+        try {
+            FileManager.createFileFromStringContent("output", filename, json.toString());
+            succeed = true;
+        }catch (Exception e){
+            println(e);
+        }
+        return succeed;
+    }
+
+    public static void println(Object o){
+        System.out.println(o);
+    }
+}
