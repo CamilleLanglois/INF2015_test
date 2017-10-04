de786f1074c53d92e1d451b6066bf57290adea48
davidboutet <dovid_16@hotmail.com>
2017-10-03 22:39:28 -0400

    Utils class created and remove all everything except main from application.java Minified method and rename variable name to camelCase format

--- src/Application.java
+++ src/Application.java
@@ -2,84 +2,18 @@
  * Created by davidboutet on 17-09-18.
  */
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import manage.file.*;
+import com.inf.Utils;
 import net.sf.json.*;
-import com.inf.Employe;
 
 public class Application {
     public static void main(String[] args){
         String filePath = args[0], outputFile = args[1];
         try{
-            JSONObject jsonObject = getJsonFromFile(filePath);
-            createEmployeFromJson(jsonObject);
-            writeJson(outputFile);
+            JSONObject jsonObject = Utils.getJsonFromFile(filePath);
+            Utils.createEmployeFromJson(jsonObject);
+            Utils.writeJson(outputFile);
         }catch (FileNotFoundException e){
-            writeErrorJson(outputFile, e.getMessage());
+            Utils.writeErrorJson(outputFile, e.getMessage());
         }
-    }
-
-    public static JSONObject getJsonFromFile(String source) throws FileNotFoundException{
-        JSONObject jsonObject;
-        try{
-            String myJSON = FileManager.createStringFromFileContent(source, "");
-            jsonObject = JSONObject.fromObject(myJSON);
-        }catch(Exception e){
-            throw new FileNotFoundException("Input json path could not be found.("+source+")");
-        }
-        return jsonObject;
-    }
-
-    //A voir si on devrait le mettre dans la classe Employe.java
-    public static void createEmployeFromJson(JSONObject jsonObject){
-        Double taux_min = Employe.stringToDouble(jsonObject.getString("taux_horaire_min")),
-               taux_max = Employe.stringToDouble(jsonObject.getString("taux_horaire_max"));
-        JSONArray employeArray = jsonObject.getJSONArray("employes");
-        for (int i = 0; i<employeArray.size(); i++){
-            JSONObject employe = (JSONObject)employeArray.get(i);
-            new Employe(employe.getString("nom"), jsonObject.getInt("type_departement"), taux_min, taux_max, employe.getInt("nombre_diplomes"), employe.getInt("nombre_droit_anciennete"), employe.getDouble("charge_travail"));
-        }
-    }
-
-    public static void writeJson(String filename){
-        JSONObject json = formatJson(Employe.finalEmployeList);
-        try {
-            FileManager.createFileFromStringContent("output", filename, json.toString());
-        }catch (Exception e){
-            println("The file could not be written.");
-        }
-    }
-
-    public static void writeErrorJson(String filename, String errorMessage){
-        try {
-            JSONObject jsonError = new JSONObject();
-            jsonError.accumulate("error", errorMessage);
-            FileManager.createFileFromStringContent("output", filename, jsonError.toString());
-        }catch (Exception e){
-            println("The file could not be written.");
-        }
-    }
-
-    public static JSONObject formatJson(ArrayList<Employe> listEmploye){
-        JSONObject json = new JSONObject();
-        JSONArray salaires = new JSONArray();
-        Double total_value = Employe.FIXED_AMOUNT,
-               total_rente_provincial = 0.00,
-               total_rente_federal = 0.00;
-        for(Employe e:listEmploye){
-            total_value += e.getTotalSalary();
-            salaires.add(e.toJSONString());
-        }
-        total_rente_provincial = Employe.calculRenteProvincial(total_value);
-        total_rente_federal = Employe.calculRenteFederal(total_value);
-        json.accumulate("valeur_total", Employe.twoDigits(Employe.roundToFive(total_value))+" $");
-        json.accumulate("rente_provinciale", Employe.twoDigits(Employe.roundToFive(total_rente_provincial))+" $");
-        json.accumulate("rente_federal", Employe.twoDigits(Employe.roundToFive(total_rente_federal))+" $");
-        json.accumulate("salaires", salaires);
-        return json;
-    }
-
-    public static void println(Object o){
-        System.out.println(o);
     }
 }
---
+++ src/com/inf/Utils.java
@@ -0,0 +1,76 @@
+package com.inf;
+
+import manage.file.FileManager;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+/**
+ * Created by davidboutet on 17-10-03.
+ */
+public class Utils {
+
+    public static JSONObject getJsonFromFile(String source) throws FileNotFoundException {
+        JSONObject jsonObject;
+        try{
+            String myJSON = FileManager.createStringFromFileContent(source, "");
+            jsonObject = JSONObject.fromObject(myJSON);
+        }catch(Exception e){
+            throw new FileNotFoundException("Input json path could not be found.("+source+")");
+        }
+        return jsonObject;
+    }
+
+    public static void createEmployeFromJson(JSONObject jsonObject){
+        Double minRate = Employe.stringToDouble(jsonObject.getString("taux_horaire_min")),
+               maxRate = Employe.stringToDouble(jsonObject.getString("taux_horaire_max"));
+        JSONArray employeArray = jsonObject.getJSONArray("employes");
+        for (Object e:employeArray){
+            JSONObject employe = (JSONObject)e;
+            new Employe(employe.getString("nom"), jsonObject.getInt("type_departement"), minRate, maxRate, employe.getInt("nombre_diplomes"), employe.getInt("nombre_droit_anciennete"), employe.getDouble("charge_travail"));
+        }
+    }
+
+    public static void writeJson(String filename){
+        JSONObject json = formatJson(Employe.finalEmployeList);
+        try {
+            FileManager.createFileFromStringContent("output", filename, json.toString());
+        }catch (Exception e){
+            println("The file could not be written.");
+        }
+    }
+
+    public static void writeErrorJson(String filename, String errorMessage){
+        try {
+            JSONObject jsonError = new JSONObject();
+            jsonError.accumulate("error", errorMessage);
+            FileManager.createFileFromStringContent("output", filename, jsonError.toString());
+        }catch (Exception e){
+            println("The file could not be written.");
+        }
+    }
+
+    public static JSONObject formatJson(ArrayList<Employe> listEmploye){
+        JSONArray salaries = new JSONArray();
+        Double totalValue = Employe.FIXED_AMOUNT;
+        for(Employe e:listEmploye){
+            totalValue += e.getTotalSalary();
+            salaries.add(e.toJSONString());
+        }
+        return addToJson(totalValue, Employe.calculRenteProvincial(totalValue), Employe.calculRenteFederal(totalValue), salaries);
+    }
+
+    public static JSONObject addToJson(Double totalValue, Double totalAnnuityProvincial, Double totalAnnuityFederal, JSONArray salaries){
+        JSONObject json = new JSONObject();
+        json.accumulate("valeur_total", Employe.twoDigits(Employe.roundToFive(totalValue))+" $");
+        json.accumulate("rente_provinciale", Employe.twoDigits(Employe.roundToFive(totalAnnuityProvincial))+" $");
+        json.accumulate("rente_federal", Employe.twoDigits(Employe.roundToFive(totalAnnuityFederal))+" $");
+        json.accumulate("salaires", salaries);
+        return json;
+    }
+
+    public static void println(Object o){
+        System.out.println(o);
+    }
+}
