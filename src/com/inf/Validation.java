2fc1e47eb841064c3d3484da2624b94d05ac6601
CamilleLanglois <camille-langlois@hotmail.com>
2017-10-05 14:49:38 -0400

    fusion hasEmploye et sizeEmployeListIsValid modification des exceptions

--- src/com/inf/Validation.java
+++ src/com/inf/Validation.java
@@ -8,6 +8,7 @@ package com.inf;
 import static com.inf.Utils.println;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.InputMismatchException;
 import net.sf.json.*;
 
 /**
@@ -43,58 +44,52 @@ public class Validation {
         }
     }
     
-    public static void workedHoursIsValid(Double workedHours) throws Exception {
+    public static void workedHoursIsValid(Double workedHours) throws IllegalArgumentException {
      
         if ((workedHours < 0) || (workedHours > 1950)) {
-            throw new Exception("Employes worked hours must be between 0 and 1950");
+            throw new IllegalArgumentException("Employes worked hours must be between 0 and 1950");
         }
 
     }
-    public static void dateIsValid(String date) throws Exception {
+    public static void dateIsValid(String date) throws InputMismatchException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         dateFormat.setLenient(false);
         try {
             dateFormat.parse(date);
         } catch (ParseException e) {
-            throw new Exception("Incorrect date format in JSON");
+            throw new InputMismatchException("Incorrect date format in JSON");
         }
     }
     
     // À regarder si je le mets avec hasEmploye et si le nom est correct.
-    public static void sizeListEmployeIsValid(JSONArray employes) throws Exception {
+    public static void employeListIsValid(JSONArray employes) throws Exception {
 
-        if (employes.size() > 10) {
+        if (employes.isEmpty()) {
+            throw new Exception("Employe list is empty");
+        } else if (employes.size() > 10) {
             throw new Exception("Employe list should be less than or equal to 10.");
         } 
 
     }
     
-    public static void hasEmploye(JSONArray employes) throws Exception {
-
-        if (employes.isEmpty()) {
-            throw new Exception("Employe list is empty");
-        } 
-
-    }
-
-    public static void objContainsAllProperties(JSONObject objet) throws Exception {
+    public static void objContainsAllProperties(JSONObject objet) throws NoSuchFieldException {
         String[] depProperties = {"nom_departement", "type_departement", "taux_horaire_min", "taux_horaire_max", "employes"};
 
         for (int i = 0; i < depProperties.length; i++) {
 
             if (!objet.containsKey(depProperties[i])) {
-                throw new Exception("Departement missing property : " + depProperties[i]);
+                throw new NoSuchFieldException("Departement missing property : " + depProperties[i]);
             }
         }
     }
 
-    public static void arrContainsAllProperties(JSONArray arr) throws Exception {
+    public static void arrContainsAllProperties(JSONArray arr) throws NoSuchFieldException {
         String[] empProperties = {"nom", "nombre_droit_anciennete", "nombre_diplomes", "charge_travail", "date_revision_salaire"};
 
         for (int i = 0; i < arr.size(); i++) {
             for (int j = 0; j < empProperties.length; j++) {
                 if (!arr.getJSONObject(i).containsKey(empProperties[j])) {
-                    throw new Exception("Employe array missing property : " + empProperties[j]);
+                    throw new NoSuchFieldException("Employe array missing property : " + empProperties[j]);
                 }
             }
         }
