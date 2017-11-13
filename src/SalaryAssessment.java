1cc362394defe7b23ee25ed70cd0e0d07fd877f5
CamilleLanglois <camille-langlois@hotmail.com>
2017-11-13 18:20:54 -0500

    ajout argument getJsonFromFile

--- src/SalaryAssessment.java
+++ src/SalaryAssessment.java
@@ -11,7 +11,7 @@ public class SalaryAssessment {
     public static void main(String[] args){
         String filePath = args[0], outputFile = args[1], historyAction = args.length>2?args[2]:"";
         try{
-            JSONObject jsonObject = Utils.getJsonFromFile(filePath);
+            JSONObject jsonObject = Utils.getJsonFromFile(filePath, true);
             Utils.createEmployeFromJson(jsonObject);
             Utils.writeJson(outputFile, Utils.formatJson(Employe.finalEmployeList));
             new History(Employe.finalEmployeList, historyAction);
